4cb262ffa8d3afbfb068ff667e538dfef8a61a43
Marcel Birkner <marcel.birkner@codecentric.de>
2013-07-05 10:28:37 +0000

    MB: added simple input validation, name needs to exist

--- server/routes/actors.js
+++ server/routes/actors.js
@@ -71,6 +71,11 @@ exports = module.exports = function (db) {
 
 
     exports.addActor = function (req, res) {
+        if (req.body.name == null || req.body.name == '' ) {
+            console.error('Name not set ' + req.body);
+            return res.status(400).send('Error: Name not set');
+        };
+
         var node = db.createNode(req.body);
         node.data.type = 'actor';
         node.data.id = uuid.v4();
