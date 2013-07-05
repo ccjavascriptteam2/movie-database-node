0f2cd39fbfa0e84ab1cd99a7ee37082ec115c790
Marcel Birkner <marcel.birkner@codecentric.de>
2013-07-05 12:16:09 +0000

    Added server side JS test for actors

--- test/server/actors.js
+++ test/server/actors.js
@@ -5,28 +5,28 @@ var expect = chai.expect;
 var neo4j = require('neo4j');
 var ResponseMock = require('./helper/response-mock');
 
-describe('getMovie', function () {
+describe('getActor', function () {
     var dbStub;
     var routes;
-    var movies;
+    var actors;
     var res;
 
     beforeEach(function() {
         dbStub = sinon.createStubInstance(neo4j.GraphDatabase);
         routes = require('../../server/routes')(dbStub);
-        movies = routes.movies;
+        actors = routes.actors;
         res = new ResponseMock();
     });
 
     it('should return an empty list when neo returns null', function (done) {
         dbStub.getIndexedNodes
-            .withArgs('node_auto_index', 'type', 'movie')
+            .withArgs('node_auto_index', 'type', 'actor')
             .yieldsAsync(
                 null,
                 null
             );
 
-        movies.getMovies({}, res);
+        actors.getActors({}, res);
 
         res.verifySend(function() {
             expect(res.statusCode).to.equal(200);
@@ -38,13 +38,13 @@ describe('getMovie', function () {
 
     it('should return an empty list when neo returns []', function (done) {
         dbStub.getIndexedNodes
-            .withArgs('node_auto_index', 'type', 'movie')
+            .withArgs('node_auto_index', 'type', 'actor')
             .yieldsAsync(
                 null,
                 null
             );
 
-        movies.getMovies({}, res);
+        actors.getActors({}, res);
 
         res.verifySend(function() {
             expect(res.statusCode).to.equal(200);
@@ -54,41 +54,27 @@ describe('getMovie', function () {
         });
     });
 
-    it('should return internal-server-error when neo returns error', function (done) {
+    it('should return a list of actors', function (done) {
         dbStub.getIndexedNodes
-            .withArgs('node_auto_index', 'type', 'movie')
-            .yieldsAsync(
-                'an error',
-                null
-            );
-
-        movies.getMovies({}, res);
-
-        res.verifySend(function() {
-            expect(res.statusCode).to.equal(500);
-            done();
-        });
-    });
-
-    it('should return a list of movies', function (done) {
-        dbStub.getIndexedNodes
-            .withArgs('node_auto_index', 'type', 'movie')
+            .withArgs('node_auto_index', 'type', 'actor')
             .yieldsAsync(
                 null,
-                [{data: {title:'Lord of the Rings', description:'Description'}},
-                 {data: {title:'Django Unchained', description:'Another Description'}}]
+                [{data: {name:'Nicole', biography:'Nicole lives in ...'}},
+                 {data: {name:'Denzel', biography:'Denzel was born in ... '}}]
             );
 
-        movies.getMovies({}, res);
+        actors.getActors({}, res);
 
         res.verifySend(function() {
             expect(res.statusCode).to.equal(200);
             expect(res.body).to.be.instanceOf(Array);
             expect(res.body).to.have.length(2);
-            expect(res.body[0]).to.have.property('title')
-                .that.equals('Lord of the Rings');
+            expect(res.body[0]).to.have.property('name')
+                .that.equals('Nicole');
             done();
         });
     });
+
+
 });
 
